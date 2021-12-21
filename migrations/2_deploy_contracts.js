a62f4b64ebb65ed5030a09aed88b3f3d95db7ed9
Clóvis Neto <clovisdasilvaneto@gmail.com>
2021-12-21 21:12:15 +0100

    move project boilerplate to github

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+// const ConvertLib = artifacts.require("ConvertLib");
+
+module.exports = function(deployer) {
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+};
